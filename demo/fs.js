7be5071ca12d2817058a4cd7f9845fd43c6c331a
SDen4 <asw325@yandex.ru>
2022-06-16 13:59:26 +0500

    create, write, re-write and append content to file

--- demo/fs.js
+++ demo/fs.js
@@ -13,4 +13,32 @@ fs.mkdir(path.join(__dirname, 'test'), (error) => {
   console.log('Fold has been created');
 });
 
+// создание нового файла
+const filePath = path.join(__dirname, 'test', 'test.txt');
+
+fs.writeFile(filePath, 'Text to new file!!!', (err) => {
+  if (err) throw err;
+
+  console.log('File has been created');
+});
+
+// перезаписть существующего файла
+fs.writeFile(filePath, 'Text to new file again!!!', (err) => {
+  if (err) throw err;
+
+  console.log('File has been re-created');
+});
+
+// добавление в существующий файл
+fs.appendFile(filePath, '\nAddition text to new row.', (err) => {
+  if (err) throw err;
+
+  console.log('File has been changed');
+});
+
 // node demo/fs
+
+// NOTES
+// writeFile может перетирать существующие файлы
+// appendFile добавляет контент в файл
+// mkdir выпадет в ошибку, если такая папка уже существует
