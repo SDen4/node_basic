e28f6b95b2e50484877a690eee537a3a7ac71a39
SDen4 <asw325@yandex.ru>
2022-06-16 11:58:52 +0500

    __filename & __dirname

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import chalk from 'chalk';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import text from './data.js';
 
 console.log(chalk.blue('hello nodejs'));
 console.log(chalk.red(text));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__dirname);
+console.log(__filename);
