ddf151ae268a7ceb0e0511f2842ddef68083f4b3
SDen4 <asw325@yandex.ru>
2022-06-16 16:45:34 +0500

    example of server rendering html pages

--- demo/server.js
+++ demo/server.js
@@ -1,11 +1,43 @@
 import http from 'http';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, {
-    'Content-Type': 'text/html',
-  });
-  // завершить ответ сервера
-  response.end('<h1>Response server!</h1>');
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'public', 'home.html'), (err, data) => {
+      if (err) throw err;
+
+      res.writeHead(200, {
+        'Content-Type': 'text/html',
+      });
+      // завершить ответ сервера
+      res.end(data);
+    });
+  } else if (req.url === '/contacts') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'contacts.html'),
+      (err, data) => {
+        if (err) throw err;
+
+        res.writeHead(200, {
+          'Content-Type': 'text/html',
+        });
+        // завершить ответ сервера
+        res.end(data);
+      },
+    );
+  } else {
+    res.end('<h1>The page not found...</h1>');
+  }
+
+  // res.writeHead(200, {
+  //   'Content-Type': 'text/html',
+  // });
+  // // завершить ответ сервера
+  // res.end('<h1>Response server!</h1>');
 });
 
 server.listen(3003, () => {
