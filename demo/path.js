4ea93c60c49414a97c652d5b587408bcdf23c852
SDen4 <asw325@yandex.ru>
2022-06-17 21:46:53 +0500

    path join additional examples

--- demo/path.js
+++ demo/path.js
@@ -2,12 +2,19 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // path methods
 console.log('Имя директории: ', path.dirname(__filename));
 console.log('Имя файла: ', path.basename(__filename));
 console.log('Расширение файла: ', path.extname(__filename));
 console.log('Parse: ', path.parse(__filename));
+
+// path join additional examples
+console.log(path.join(__filename, 'first', 'second', 'third'));
+console.log(path.join(__dirname, 'first', 'second', 'third'));
+console.log(path.join(__dirname, '..'));
+console.log(path.join(__dirname, '..', '..'));
 
 // get name example
 const name = path.parse(__filename).name;
