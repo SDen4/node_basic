9f141fbddcb5a4aeb8d94267b0552bf8e1ddab6a
SDen4 <asw325@yandex.ru>
2022-06-20 16:08:31 +0500

    Post schema

--- demo/express/index.js
+++ demo/express/index.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import Post from './Post.js';
+
 const PORT = 5000;
 const DB_URL = `mongodb+srv://user:user@cluster0.rvkbuaq.mongodb.net/?retryWrites=true&w=majority`;
 
 const app = express();
 app.use(express.json());
 
-app.post('/', (req, res) => {
-  console.log(req.body);
-  res.status(200).json('server is working!');
+app.post('/', async (req, res) => {
+  try {
+    const { author, title, content, picture } = req.body;
+    const post = await Post.create({ author, title, content, picture });
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
 async function startApp() {
