f139b28a3c3355a8f79aac8c1362868cfa3b4be4
SDen4 <asw325@yandex.ru>
2022-06-20 16:23:45 +0500

    router in express

--- demo/express/index.js
+++ demo/express/index.js
@@ -1,24 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
-import Post from './Post.js';
+import router from './router.js';
 
 const PORT = 5000;
 const DB_URL = `mongodb+srv://user:user@cluster0.rvkbuaq.mongodb.net/?retryWrites=true&w=majority`;
 
 const app = express();
 app.use(express.json());
-
-app.post('/', async (req, res) => {
-  try {
-    const { author, title, content, picture } = req.body;
-    const post = await Post.create({ author, title, content, picture });
-
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(500).json(err);
-  }
-});
+app.use('/api', router);
 
 async function startApp() {
   try {
---
+++ demo/express/router.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import Post from './Post.js';
+
+const router = new Router();
+
+router.post('/posts', async (req, res) => {
+  try {
+    const { author, title, content, picture } = req.body;
+    const post = await Post.create({ author, title, content, picture });
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
+router.get('/posts');
+router.get('/posts/:id');
+router.put('/posts');
+router.delete('/posts/:id');
+
+export default router;
