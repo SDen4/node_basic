ea679a62d098a7481ae33dda04574812ede3a0c2
SDen4 <asw325@yandex.ru>
2022-06-20 16:53:14 +0500

    controller

---
+++ demo/express/PostController.js
@@ -0,0 +1,16 @@
+import Post from './Post.js';
+
+class PostController {
+  async create(req, res) {
+    try {
+      const { author, title, content, picture } = req.body;
+      const post = await Post.create({ author, title, content, picture });
+
+      res.status(200).json(post);
+    } catch (error) {
+      res.status(500).json(err);
+    }
+  }
+}
+
+export default new PostController();
--- demo/express/router.js
+++ demo/express/router.js
@@ -1,18 +1,9 @@
 import { Router } from 'express';
-import Post from './Post.js';
+import PostController from './PostController.js';
 
 const router = new Router();
 
-router.post('/posts', async (req, res) => {
-  try {
-    const { author, title, content, picture } = req.body;
-    const post = await Post.create({ author, title, content, picture });
-
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(500).json(err);
-  }
-});
+router.post('/posts', PostController.create);
 router.get('/posts');
 router.get('/posts/:id');
 router.put('/posts');
