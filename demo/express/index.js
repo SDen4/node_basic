2fde84afc1de1edc6ee49d2e4f758129d7d6680a
SDen4 <asw325@yandex.ru>
2022-06-20 14:23:45 +0500

    apply mongoose

--- demo/express/index.js
+++ demo/express/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const PORT = 5000;
+const DB_URL = `mongodb+srv://user:user@cluster0.rvkbuaq.mongodb.net/?retryWrites=true&w=majority`;
 
 const app = express();
 app.use(express.json());
@@ -10,10 +12,22 @@ app.post('/', (req, res) => {
   res.status(200).json('server is working!');
 });
 
-app.listen(PORT, () => {
-  console.log(
-    `express example start on port ${PORT}. Open http://localhost:5000/`,
-  );
-});
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    app.listen(PORT, () => {
+      console.log(
+        `express example start on port ${PORT}. Open http://localhost:5000/`,
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startApp();
 
 // npm start
